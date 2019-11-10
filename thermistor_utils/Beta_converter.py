a28b8acd94ea9210aee49e5deb757554a0e42038
iuz <giampaolo.eusebi@gmail.com>
2019-11-10 15:42:02 +0100

    Add to_cstr and refactor python printing

--- thermistor_utils/Beta_converter.py
+++ thermistor_utils/Beta_converter.py
@@ -13,7 +13,7 @@ class Beta_converter(object):
     """
     __slots__ = ("beta", "R0", "T0", "T1", )
 
-    def __init__(self, beta, R0, T0, T1):
+    def __init__(self, beta, R0, T0, T1=None):
         """ Create a converter where `beta` is the beta value in
         Kelvin and `R0` is the resistance at 25 degrees Celsius.
         `T0` and `T1` are respectively the temperature references
@@ -50,16 +50,43 @@ class Beta_converter(object):
 
         return R
 
+    def _to_str_impl(self, with_temps=True):
+        """
+        Stringify (kinda) implementation used by
+        `__repr__`, `__str__` and `to_cstr`.
+        """
+        beta, R0, T0, T1 = self.beta, self.R0, self.T0, self.T1
+
+        if with_temps and T1 is not None:
+            return f"{beta}, {R0}, {T0}, {T1}"
+
+        return f"{beta}, {R0}, {T0}"
+
+    def to_cstr(self, with_temps=True):
+        """
+        Return a string with the beta values and, if available, the
+        temperatures provided upon creation.
+
+        Use `with_temp` to control whether the low/high temperature
+        should be printed.
+        """
+        s = self._to_str_impl(with_temps)
+        return f"{{{s}}}"
+
     def __repr__(self):
-        return (
-            f"Beta_converter("
-            f"beta={self.beta:g}, R0={self.R0:g}, "
-            f"T0={self.T0:g}, T1={self.T1:g})"
-        )
+        """
+        Tentatively print the converter to satisfy
+
+            converter = eval(converter)
+
+        Where equality means different objects but same functionality.
+        """
+        s = self._to_str_impl(with_temps=True)
+        return f"Beta_converter({s})"
 
     def __str__(self):
-        return (
-            f"Beta_converter["
-            f"B={self.beta}K at {self.R0} "
-            f"{self.T0}/{self.T1}]"
-        )
+        """
+        Create a string representation of the object.
+        """
+        s = self._to_str_impl(with_temps=True)
+        return f"Beta_converter[{s}]"
