6a4028c07680f905704debe2fd15a449f8155e4f
iuz <giampaolo.eusebi@gmail.com>
2019-12-09 05:50:01 +0100

    Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name='thermistor_utils',
-    version='0.0.1',
+    version='0.0.2',
     packages=('thermistor_utils', ),
 
     description=short_desc,
