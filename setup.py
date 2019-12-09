15852aa512762652e609b9da10ab4ee65b0392dd
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2019-12-09 08:52:46 +0100

    Make image links URI

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import re
 
 short_desc = (
     'Steinhart -- Hart and Beta thermistor temperature model in '
@@ -6,11 +7,16 @@ short_desc = (
 )
 
 with open("README.md") as fh:
-    long_desc = fh.read()
+    raw_url = 'https://gitlab.com/geusebi/thermistor-utils/raw/master/'
+
+    ref_re = re.compile("\[(.+?)]: *(.+?)")
+    img_re = re.compile("!\[(.+?)] *\((.+?)\)")
+    long_desc = ref_re.sub(f"[\\1]: {raw_url}\\2", fh.read())
+    long_desc = img_re.sub(f"![\\1]({raw_url}\\2)", long_desc)
 
 setuptools.setup(
     name='thermistor_utils',
-    version='0.0.2',
+    version='0.0.3',
     packages=('thermistor_utils', ),
 
     description=short_desc,
@@ -27,12 +33,12 @@ setuptools.setup(
     author_email='giampaolo.eusebi@gmail.com',
 
     license='GNU LGPL 3.0',
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Manufacturing',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved', #?
+        'License :: OSI Approved',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: C',
@@ -42,5 +48,5 @@ setuptools.setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Embedded Systems',
         'Topic :: Utilities',
-    ),
+    ],
 )
